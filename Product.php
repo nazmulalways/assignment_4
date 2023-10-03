cdc07766c699bfb596960bdcff04ea7d9d2d9372
nazmulalways <144802510+nazmulalways@users.noreply.github.com>
2023-10-03 19:15:46 +0600

    Update Product.php

--- Product.php
+++ Product.php
@@ -1,6 +1,5 @@
 <?php 
 class Product {
-    // Properties
     private $id;
     private $name;
     private $price;
@@ -11,25 +10,14 @@ class Product {
         $this->name = $name;
         $this->price = $price;
     }
-
-
-    // Getter methods
-    public function getId() {
-        return $this->id;
-    }
-
-    public function getName() {
-        return $this->name;
-    }
-
     public function getFormattedPrice() {
         return (string) $this->price;
     }
     
     public function showDetails() {
         echo "Product Details:".PHP_EOL;
-        echo '- ID: ' . $this->getId() . PHP_EOL;
-        echo '- Name: ' . $this->getName() . PHP_EOL;
+        echo '- ID: ' . $this->id . PHP_EOL;
+        echo '- Name: ' . $this->name . PHP_EOL;
         echo '- Price: $' . $this->getFormattedPrice() . PHP_EOL;
     }
 }
